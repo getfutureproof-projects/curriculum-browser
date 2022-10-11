d968331d3f4692c49af158af5166eff3a675f9c0
Gingertonic <beth@getfutureproof.co.uk>
2022-10-11 14:39:04 +0100

    correct foundation description

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CourseSection } from './components/CourseSection'
 
 function App() {
   const courseSections = [
-    { name: "Foundations", atId: "shrdM3hKaMLUnLRGm", description: "Gain insight into the landscape of modern tech industry and ability to apply key professional development skills" },
+    { name: "Foundations", atId: "shrdM3hKaMLUnLRGm", description: "Gain insight into the landscape of the modern tech industry and ability to apply key professional development & communication skills" },
     { name: "Discovery", atId: "shrUolFJ0sCdpGEEA", description: "Gain essential understanding of & ability to apply basic techniques in key technology areas" },
     { name: "Disciplines", description: "Gain deeper understanding & ability to apply further techniques that are particularly desirable within the area of assigned discipline" },
     { name: "Launch", atId: "shrdjfcG9cqQDX92N", description: "Gain understanding of interdisciplinary communication & collaboration and combine skills into a long-form multidisciplinary project" }
